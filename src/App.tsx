a3a75eb9bb14ec1521c457aa3654afc8688b1e11
FoxWithMatches <freiheiit92@mail.ru>
2022-12-24 08:42:29 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextContent from './textContent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <TextContent />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
