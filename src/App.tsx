156168f524161837c5a9a175f37e1cf765ae601a
FoxWithMatches <freiheiit92@mail.ru>
2023-01-16 17:01:06 +0300

    header + notfound

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Main } from 'Containers/Main';
+import { NotFound } from 'Containers/NotFound';
 import { Posts } from 'Containers/Posts';
 import { routes } from 'Helpers/Constants/routes';
 import React from 'react';
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path={routes.main} element={<Main />} />
       <Route path={routes.posts} element={<Posts />} />
+      <Route path={routes.notFound} element={<NotFound/>} />
     </Routes>
   );
 }
--- src/Components/Common/Header/index.tsx
+++ src/Components/Common/Header/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { routes } from 'Helpers/Constants/routes';
+import { Link } from 'react-router-dom';
 import style from './Header.module.scss';
 
 export const Header = () => {
   return (
     <header className={style.header}>
       <span className={style.header_logo}>Logo</span>
+      <ul>
+        <li>
+          <Link to={routes.main}>Главная</Link>
+        </li>
+        <li>
+          <Link to={routes.posts}>Work</Link>
+        </li>
+        <li>
+          <Link to="/about">Главная</Link>
+        </li>
+      </ul>
       <span className={style.header_title}>App_name</span>
     </header>
   );
